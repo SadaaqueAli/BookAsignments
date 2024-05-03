1816629f11739547d7e137f67683d1deaaac870c
SadaaqueAli <sadaaqueali@gmail.com>
2024-05-03 08:15:24 +0500

    chapter 1,2,3

---
+++ app.js
@@ -0,0 +1,56 @@
+/*asignment==chapter=01====book-======================================--java=========
+function greetVisitor() {
+    alert("Error! Please inter a valid password.");
+}
+window.onload = greetVisitor;*/
+
+/*asignment==chapter=02====book--=================-java======================================================
+var username;
+
+var myName = "Your Full Name";
+
+var message;
+message = "Hello World";
+alert(message);
+
+var studentName = "Sadaqat Ali";
+var age = 20;
+var gender = "Male";
+var country = "Pakistan";
+
+alert("Student Name: " + studentName);
+alert("Age: " + age);
+alert("Gender: " + gender);
+alert("Country: " + country);
+
+var pizzaPattern = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizzaPattern); */
+
+/*chapter 2 asignments ++++++++++++++++++2+++++++++++++++++++++++++++
+
+var email ="sadaaqueali@gmail.com";
+alert ("Your email Address is :" + email);
+
+==2=====
+
+var book = "A smarter way to learn Javascript.";
+alert ("Im tring to learn from :" + book);
+
+===3====
+
+var siginLine
+siginLine = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert (siginLine);*/
+
+/*chapter 3 asignments ++++++++++++++++++2++++++++++3+++++++++++++++++
+
+var age = ("Im 15 yaers old");
+alert (age); */
+
+var age =(19)
+if(age>18){
+    alert("yes boy you can drive")
+}
+else{
+    alert("no boy youcan not drive")
+}
